89afa145ad2fc6636d3592c8496ae8713439b7bb
Diamon <dcpho33@live.com>
2021-06-18 13:39:15 +0800

    init

---
+++ src/main/java/com/dc/vediojc/VediojcApplication.java
@@ -0,0 +1,13 @@
+package com.dc.vediojc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VediojcApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(VediojcApplication.class, args);
+    }
+    
+}
